d878dd2b6a5cbfde2f5db87ad9e83cdb4c913f58
Daria Vdovina <vdovinadn@gmail.com>
2024-04-04 19:32:14 +0300

    build: .env

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,13 +4,19 @@ import { Wishlist } from 'src/wishlists/entities/wishlist.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Offer } from 'src/offers/entities/offer.entity';
 
+const { POSTGRES_HOST = "localhost",
+  POSTGRES_PORT = "5432",
+  POSTGRES_USER = "student",
+  POSTGRES_PASSWORD = "student",
+  POSTGRES_DB = "kupipodariday" } = process.env;
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'student',
-  password: 'student',
-  database: 'kupipodariday',
+  host: POSTGRES_HOST,
+  port: +POSTGRES_PORT,
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DB,
   entities: [Wish, Wishlist, User, Offer],
   synchronize: true,
 };
